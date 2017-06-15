584e38bb6661e3fb1db814c8cc964d5d66985275
charles <charlesrymal@gmail.com>
2017-06-15 11:58:51 -0700

    minor

--- modconf/__init__.py
+++ modconf/__init__.py
@@ -2,8 +2,12 @@ import sys
 
 def import_conf(name, folder=None):
     sys.path.insert(0, folder)
-
-    m = __import__(name, fromlist='*')
+    
+    try:
+        m = __import__(name, fromlist='*')
+    except:
+        print('modconf folder={}'.format(repr(folder)))
+        raise
 
     sys.path.pop(0)
 
