f094e6edc7ab5c9fe981cb08ccd6c9c2fff833b1
charles rymal <charlesrymal@gmail.com>
2017-06-21 18:26:35 -0700

    use pkgtool

--- setup.py
+++ setup.py
@@ -1,21 +1,9 @@
-
-import re
+import os
 from setuptools import setup
 
-with open('modconf/__init__.py') as f:
-    version = re.findall("^__version__ = '(.*)'", f.read())[0]
+import pkgtool
 
-setup(name='modconf',
-        version=version,
-        description='pattern for using python modules as configuration files',
-        url='http://github.com/chuck1/modconf',
-        author='Charles Rymal',
-        author_email='charlesrymal@gmail.com',
-        license='MIT',
-        packages=(
-            'modconf',
-            'modconf.tests'),
-        package_data={'': ['*.txt']},
-        zip_safe=False,
-        )
+pkg = pkgtool.Package(os.getcwd())
+
+setup(**pkg.setup_args())
 
